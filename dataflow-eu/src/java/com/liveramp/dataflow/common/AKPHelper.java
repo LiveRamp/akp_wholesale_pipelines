7eee9b9582a914e90d61df172bd5cbc5231ed5a0
jkumav <jkumav.@liveramp.com>
2020-12-10 18:18:54 +0000

    CR comments resolved

--- dataflow-eu/src/java/com/liveramp/dataflow/common/AKPHelper.java
+++ dataflow-eu/src/java/com/liveramp/dataflow/common/AKPHelper.java
@@ -7,17 +7,11 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import com.liveramp.dataflow.akp.AkpLoadingOptions;
 
 public class AKPHelper {
-
   public static final String KEY_VALUE_SEPARATOR = "=";
   public static final String BIGTABLE_SEPARATOR = "#";
   public static final String PREFERRED_PEL_KEY = "LRI_DER_LRI_SHA256_EMAIL1";
-
-  public static final String INSTANCE_ID = "pixel-serving";
-  public static final String PROJECT_ID = "eu-central-prod";
   public static final String COLUMN_FAMILY = "pel";
   public static final String COLUMN_QUALIFIER = "pel";
-  public static final String ARL_DIFF_TABLE = "arl_diff";
-  public static final String ARL_PEL_TABLE = "arl_to_pel";
 
   public static CloudBigtableTableConfiguration getArlPelBigtableConfig(AkpLoadingOptions akpLoadingOptions) {
     GcpOptions gcpOptions = PipelineOptionsFactory.as(GcpOptions.class);
